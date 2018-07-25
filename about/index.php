b56f1c40f3acc27a08fd02a87f4dfe45397e1096
kvesskrishna <krishna.kkarthi@gmail.com>
2018-07-25 11:22:41 +0530

    added templates

---
+++ about/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+
+  <!-- Meta Tags -->
+  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <meta name="description" content="KPIAS" />
+  <meta name="keywords" content="education,school,university,educational,learn,learning,teaching,workshop" />
+  <meta name="author" content="KPIAS, 21st Century Software Solutions" />
+
+  <!-- Page Title -->
+  <title>KPIAS - Krishna Pradeep's IAS</title>
+
+  <!-- Favicon and Touch Icons -->
+  <link href="../images/logo.png" rel="shortcut icon" type="image/png">
+  <link href="../images/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="../images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+  <link href="../images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+  <link href="../images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+
+  <!-- Stylesheet -->
+  <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="../css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+  <link href="../css/animate.css" rel="stylesheet" type="text/css">
+  <link href="../css/css-plugin-collections.css" rel="stylesheet" />
+  <!-- CSS | menuzord megamenu skins -->
+  <link href="../css/menuzord-megamenu.css" rel="stylesheet" />
+  <link id="menuzord-menu-skins" href="../css/menuzord-skins/menuzord-boxed.css" rel="stylesheet" />
+  <!-- CSS | Main style file -->
+  <link href="../css/style-main.css" rel="stylesheet" type="text/css">
+  <!-- CSS | Preloader Styles -->
+  <link href="../css/preloader.css" rel="stylesheet" type="text/css">
+  <!-- CSS | Custom Margin Padding Collection -->
+  <link href="../css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
+  <!-- CSS | Responsive media queries -->
+  <link href="../css/responsive.css" rel="stylesheet" type="text/css">
+  <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+  <!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
+
+  <!-- Revolution Slider 5.x CSS settings -->
+  <link href="../js/revolution-slider/css/settings.css" rel="stylesheet" type="text/css" />
+  <link href="../js/revolution-slider/css/layers.css" rel="stylesheet" type="text/css" />
+  <link href="../js/revolution-slider/css/navigation.css" rel="stylesheet" type="text/css" />
+
+  <!-- CSS | Theme Color -->
+  <link href="../css/colors/theme-skin-color-set1.css" rel="stylesheet" type="text/css">
+
+  <!-- external javascripts -->
+  <script src="../js/jquery-2.2.4.min.js"></script>
+  <script src="../js/jquery-ui.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <!-- JS | jquery plugin collection for this theme -->
+  <script src="../js/jquery-plugin-collection.js"></script>
+
+  <!-- Revolution Slider 5.x SCRIPTS -->
+  <script src="../js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
+  <script src="../js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!-- page level styles -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> 
+</head>
+
+<body class="has-side-panel side-panel-right fullwidth-page">
+  <div id="wrapper" class="clearfix">
+    <!-- preloader -->
+    <?php include '../preloader/preloader.php';?>
+    <!-- Header -->
+    <?php include '../header/header.php';?>
+    <!-- Start main-content -->
+    <div class="main-content">
+
+<!--ACTUAL PAGE CONTENT GOES HERE-->
+    <!-- Section: inner-header -->
+    <section class="inner-header divider layer-overlay overlay-theme-colored-7" data-bg-img="../images/bg/bg1.jpg">
+      <div class="container pt-120 pb-60">
+        <!-- Section Content -->
+        <div class="section-content">
+          <div class="row"> 
+            <div class="col-md-6">
+              <h2 class="text-theme-colored2 font-36">About</h2>
+              <ol class="breadcrumb text-left mt-10 white">
+                <li><a href="#">Home</a></li>
+                <li class="active">About</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Section About -->
+    <section>
+      <div class="container">
+        <div class="section-content">
+          <div class="row">
+            <div class="col-md-6">
+              <h2 class="text-uppercasetext-theme-colored mt-0 mb-0 mt-sm-30">Welcome to <span class="text-theme-colored2">KPIAS</span></h2>
+              <h4 class="mt-5 mb-15">Providing Leading Civils/Groups Coaching </h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas voluptatem maiores eaque similique non distinctio voluptates perspiciatis omnisrepell endus ipsa aperiam, laudantium voluptatum nulla.</p>
+              <p class="hidden-md">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta assumenda voluptatum ibero eligendi molestias iure error animi totam laudantium aspern similique id eos at consectetur illo culpa, quisquam Quas voluptatem maiores eaque similique</p>
+            </div>
+            <div class="col-md-6">
+              <img class="img-fullwidth" src="../images/about/1.jpg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Section mission -->
+    <section>
+      <div class="container pt-20">
+        <div class="section-content">
+          <div class="row">
+            <div class="col-md-6">
+              <img class="img-fullwidth hidden-md" src="../images/about/2.png" alt="">
+              <img class="img-fullwidth hidden-xs hidden-sm hidden-lg" src="images/about/1.png" alt="">
+            </div>
+            <div class="col-md-6">
+              <h2 class="text-uppercasetext-theme-colored mt-0 mt-sm-30">Our <span class="text-theme-colored2">Misson</span></h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas voluptatem maiores eaque similique non distinctio voluptates perspiciatis omnis, repellendus ipsa aperiam, laudantium voluptatum nulla?.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos ipsum voluptate numquam, iure odio commodi rerum quasi distinctio qui eligendi similique eius consequatur modi! Voluptas quam consequatur, debitis recusandae facilis, autem in! Enim laudantium rem, placeat odit esse numquam facere ut. Quae, minus dolorum corrupti!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Divider: Funfact -->
+    <section class="layer-overlay overlay-theme-colored-9 parallax" data-bg-img="../images/bg/bg1.jpg">
+      <div class="container pt-80 pb-90 pt-md-70 pb-md-50 pb-sm-50">
+        <div class="row mt-30 text-center">
+          <div class="col-xs-12 col-sm-6 col-md-3 mb-md-50">
+            <div class="funfact">
+              <i class="pe-7s-smile mb-20 text-white"></i>
+              <h2 data-animation-duration="2000" data-value="10" class="animate-number text-theme-colored2 font-42 font-weight-600 mt-0 mb-15">0</h2>
+              <h5 class="text-white text-uppercase">Campus Locations</h5>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-3 mb-md-50">
+            <div class="funfact">
+              <i class="pe-7s-notebook mb-20 text-white"></i>
+              <h2 data-animation-duration="2000" data-value="15" class="animate-number text-theme-colored2 font-42 font-weight-600 mt-0 mb-15">0</h2>
+              <h5 class="text-white text-uppercase">Years of Experience</h5>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-3 mb-md-50">
+            <div class="funfact">
+              <i class="pe-7s-users mb-20 text-white"></i>
+              <h2 data-animation-duration="2000" data-value="3000" class="animate-number text-theme-colored2 font-42 font-weight-600 mt-0 mb-15">0</h2>
+              <h5 class="text-white text-uppercase">Selections</h5>
+            </div>
+          </div>
+          <div class="col-xs-12 col-sm-6 col-md-3 mb-md-50">
+            <div class="funfact">
+              <i class="pe-7s-study mb-20 text-white"></i>
+              <h2 data-animation-duration="2000" data-value="15000" class="animate-number text-theme-colored2 font-42 font-weight-600 mt-0 mb-15">0</h2>
+              <h5 class="text-white text-uppercase">Students</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Section: team -->
+    <section id="team">
+      <div class="container">
+        <div class="section-title mb-40">
+          <div class="row">
+            <div class="col-md-12">
+              <h2 class="title text-uppercase mb-5">Our <span class="text-theme-colored2">Director</span></h2>
+              <h5 class="font-16 mt-5">Our Inspiring Leader</h5>
+            </div>
+          </div>
+        </div>
+        <div class="section-content">
+          <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+              <div class="team-members mb-40">
+                <div class="team-thumb pull-left team-pull-none flip mr-sm-0 t-mr-0">
+                  <img src="../images/team/1.jpg" alt="" class="img-fullwidth">
+                </div>
+                <div class="team-bottom-part border-1px p-15">
+                  <h4 class="text-uppercase font-weight-600 m-0 pb-5">Krishna Pradeep Sir</h4>
+                  <h6 class="font-13 text-gray mt-0">Director, Civils Trainer</h6>
+                  <ul class="list-inline mt-15">
+                    <li class="m-0 pr-10"> <i class="fa fa-phone text-theme-colored2 mr-5"></i> <a class="text-gray" href="#">123-456-789</a> </li>
+                    <li class="m-0 pr-10"> <i class="fa fa-envelope-o text-theme-colored2 mr-5"></i> <a class="text-gray" href="#">kptrainer@gmail.com</a> </li>
+                    <li class="m-0 pr-10"> <i class="fa fa-globe text-theme-colored2 mr-5"></i> <a class="text-gray" href="#">www.kpias.com</a> </li>
+                  </ul>
+                  <div style="padding: 15px;">
+                  <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas voluptatem maiores eaque similique non distinctio voluptates perspiciatis omnisrepell endus ipsa aperiam, laudantium voluptatum nulla.
+<br>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta assumenda voluptatum ibero eligendi molestias iure error animi totam laudantium aspern similique id eos at consectetur illo culpa, quisquam Quas voluptatem maiores eaque similique
+                  </p>
+                  </div>
+                  
+                  <ul class="styled-icons icon-sm icon-dark icon-theme-colored2 mt-15">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    </div>
+    <!-- end main-content -->
+    <!-- Footer -->
+    <?php include '../footer/footer.php';?>      
+
+  </div>
+  <!-- end wrapper -->
+
+  <!-- Footer Scripts -->
+  <!-- JS | Chart-->
+  <script src="../js/chart.js"></script>
+  <!-- JS | Custom script for all pages -->
+  <script src="../js/custom.js"></script>
+
+  <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  
+      (Load Extensions only on Local File Systems ! 
+       The following part can be removed on Server for On Demand Loading) -->
+  <script src="../js/revolution-slider/js/extensions/revolution.extension.actions.min.js"></script>
+  <script src="../js/revolution-slider/js/extensions/revolution.extension.carousel.min.js"></script>
+  <script src="../js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js"></script>
+  <script src="../js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js"></script>
+  <script src="../js/revolution-slider/js/extensions/revolution.extension.migration.min.js"></script>
+  <script src="../js/revolution-slider/js/extensions/revolution.extension.navigation.min.js"></script>
+  <script src="../js/revolution-slider/js/extensions/revolution.extension.parallax.min.js"></script>
+  <script src="../js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js"></script>
+  <script src="../js/revolution-slider/js/extensions/revolution.extension.video.min.js"></script>
+
+</body>
+
+</html>
