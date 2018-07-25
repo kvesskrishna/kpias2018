8b8670937f8ec6acad296b1ac20a3140ae62f00c
kvesskrishna <krishna.kkarthi@gmail.com>
2018-07-25 18:02:06 +0530

    contact page

---
+++ contact/index.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+
+    <!-- Meta Tags -->
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta name="description" content="KPIAS" />
+    <meta name="keywords" content="education,school,university,educational,learn,learning,teaching,workshop" />
+    <meta name="author" content="KPIAS, 21st Century Software Solutions" />
+
+    <!-- Page Title -->
+    <title>KPIAS - Krishna Pradeep's IAS</title>
+
+    <!-- Favicon and Touch Icons -->
+    <link href="../images/logo.png" rel="shortcut icon" type="image/png">
+    <link href="../images/apple-touch-icon.png" rel="apple-touch-icon">
+    <link href="../images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+    <link href="../images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+    <link href="../images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+
+    <!-- Stylesheet -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+    <link href="../css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+    <link href="../css/animate.css" rel="stylesheet" type="text/css">
+    <link href="../css/css-plugin-collections.css" rel="stylesheet" />
+    <!-- CSS | menuzord megamenu skins -->
+    <link href="../css/menuzord-megamenu.css" rel="stylesheet" />
+    <link id="menuzord-menu-skins" href="../css/menuzord-skins/menuzord-boxed.css" rel="stylesheet" />
+    <!-- CSS | Main style file -->
+    <link href="../css/style-main.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Preloader Styles -->
+    <link href="../css/preloader.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Custom Margin Padding Collection -->
+    <link href="../css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Responsive media queries -->
+    <link href="../css/responsive.css" rel="stylesheet" type="text/css">
+    <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+    <!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
+
+    <!-- Revolution Slider 5.x CSS settings -->
+    <link href="../js/revolution-slider/css/settings.css" rel="stylesheet" type="text/css" />
+    <link href="../js/revolution-slider/css/layers.css" rel="stylesheet" type="text/css" />
+    <link href="../js/revolution-slider/css/navigation.css" rel="stylesheet" type="text/css" />
+
+    <!-- CSS | Theme Color -->
+    <link href="../css/colors/theme-skin-color-set1.css" rel="stylesheet" type="text/css">
+
+    <!-- external javascripts -->
+    <script src="../js/jquery-2.2.4.min.js"></script>
+    <script src="../js/jquery-ui.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <!-- JS | jquery plugin collection for this theme -->
+    <script src="../js/jquery-plugin-collection.js"></script>
+
+    <!-- Revolution Slider 5.x SCRIPTS -->
+    <script src="../js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
+    <script src="../js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+    <!-- page level styles -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+</head>
+
+<body class="has-side-panel side-panel-right fullwidth-page">
+    <div id="wrapper" class="clearfix">
+        <!-- preloader -->
+        <?php include '../preloader/preloader.php';?>
+        <!-- Header -->
+        <?php include '../header/header.php';?>
+        <!-- Start main-content -->
+        <div class="main-content">
+
+            <!--ACTUAL PAGE CONTENT GOES HERE-->
+            <!-- Section: inner-header -->
+            <section class="inner-header divider layer-overlay overlay-theme-colored-7" data-bg-img="../images/bg/bg1.jpg">
+                <div class="container pt-120 pb-60">
+                    <!-- Section Content -->
+                    <div class="section-content">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <h2 class="text-theme-colored2 font-36">Contact</h2>
+                                <ol class="breadcrumb text-left mt-10 white">
+                                    <li>
+                                        <a href="#">Home</a>
+                                    </li>
+                                    <li class="active">Contact</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- Divider: Contact -->
+            <section class="divider">
+                <div class="container">
+                    <div class="row pt-30">
+                        <div class="col-md-6">
+                            <h3 class="line-bottom mt-0 mb-30">Interested in our course?</h3>
+
+                            <!-- Contact Form -->
+                            <form id="contact_form" name="contact_form" class="" action="" method="post">
+
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label>Name
+                                                <small>*</small>
+                                            </label>
+                                            <input name="form_name" class="form-control" type="text" placeholder="Enter Name" required="">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label>Email
+                                                <small>*</small>
+                                            </label>
+                                            <input name="form_email" class="form-control required email" type="email" placeholder="Enter Email">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label>Subject
+                                                <small>*</small>
+                                            </label>
+                                            <input name="form_subject" class="form-control required" type="text" placeholder="Enter Subject">
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <div class="form-group">
+                                            <label>Phone</label>
+                                            <input name="form_phone" class="form-control" type="text" placeholder="Enter Phone">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label>Message</label>
+                                    <textarea name="form_message" class="form-control required" rows="5" placeholder="Enter Message"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <input name="form_botcheck" class="form-control" type="hidden" value="" />
+                                    <button type="submit" class="btn btn-dark btn-theme-colored btn-flat mr-5" data-loading-text="Please wait...">Send your message</button>
+                                    <button type="reset" class="btn btn-default btn-flat btn-theme-colored">Reset</button>
+                                </div>
+                            </form>
+
+                            <!-- Contact Form Validation-->
+                            <script type="text/javascript">
+                                $("#contact_form").validate({
+                                    submitHandler: function (form) {
+                                        var form_btn = $(form).find('button[type="submit"]');
+                                        var form_result_div = '#form-result';
+                                        $(form_result_div).remove();
+                                        form_btn.before(
+                                            '<div id="form-result" class="alert alert-success" role="alert" style="display: none;"></div>'
+                                        );
+                                        var form_btn_old_msg = form_btn.html();
+                                        form_btn.html(form_btn.prop('disabled', true).data("loading-text"));
+                                        $(form).ajaxSubmit({
+                                            dataType: 'json',
+                                            success: function (data) {
+                                                if (data.status == 'true') {
+                                                    $(form).find('.form-control').val('');
+                                                }
+                                                form_btn.prop('disabled', false).html(
+                                                    form_btn_old_msg);
+                                                $(form_result_div).html(data.message).fadeIn(
+                                                    'slow');
+                                                setTimeout(function () {
+                                                    $(form_result_div).fadeOut('slow')
+                                                }, 6000);
+                                            }
+                                        });
+                                    }
+                                });
+                            </script>
+                        </div>
+                        <div class="col-md-6">
+                            <h3 class="line-bottom mt-0">Get in touch with us</h3>
+                            <p>Aliquam officia dolor rerum enim doloremque iusto eos atque tempora dignissimos similique, quae,
+                                maxime sit accusantium delectus.</p>
+                            <ul class="styled-icons icon-dark icon-sm icon-circled mb-20">
+                                <li>
+                                    <a href="#" data-bg-color="#3B5998">
+                                        <i class="fa fa-facebook"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" data-bg-color="#02B0E8">
+                                        <i class="fa fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" data-bg-color="#D71619">
+                                        <i class="fa fa-google-plus"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" data-bg-color="#A4CA39">
+                                        <i class="fa fa-android"></i>
+                                    </a>
+                                </li>
+                            </ul>
+
+                            <div class="icon-box media mb-15">
+                                <a class="media-left pull-left flip mr-20" href="#">
+                                    <i class="pe-7s-map-2 text-theme-colored"></i>
+                                </a>
+                                <div class="media-body">
+                                    <h5 class="mt-0">Our Head Office</h5>
+                                    <p>Opp : Sub Registrar Office, Ashok Nagar, Hyderabad-500020. </p>
+                                </div>
+                            </div>
+                            <div class="icon-box media mb-15">
+                                <a class="media-left pull-left flip mr-15" href="#">
+                                    <i class="pe-7s-call text-theme-colored"></i>
+                                </a>
+                                <div class="media-body">
+                                    <h5 class="mt-0">Contact Number</h5>
+                                    <p>
+                                        <a href="tel:+325-12345-65478"> 040 66787733</a>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="icon-box media mb-15">
+                                <a class="media-left pull-left flip mr-15" href="#">
+                                    <i class="pe-7s-mail text-theme-colored"></i>
+                                </a>
+                                <div class="media-body">
+                                    <h5 class="mt-0">Email Address</h5>
+                                    <p>
+                                        <a href="mailto:supporte@yourdomin.com">info@kpias.com</a>
+                                    </p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- Divider: Google Map -->
+            <section>
+                <div class="container-fluid pt-0 pb-0">
+                    <div class="row">
+                    <div id="map" style="height: 400px; width: 100%;">
+                    </div>
+                </div>
+            </section>
+        </div>
+        <!-- end main-content -->
+        <!-- Footer -->
+        <?php include '../footer/footer.php';?>
+
+    </div>
+    <!-- end wrapper -->
+
+    <!-- Footer Scripts -->
+    <!-- JS | Chart-->
+    <script src="../js/chart.js"></script>
+    <!-- JS | Custom script for all pages -->
+    <script src="../js/custom.js"></script>
+
+    <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  
+      (Load Extensions only on Local File Systems ! 
+       The following part can be removed on Server for On Demand Loading) -->
+    <script src="../js/revolution-slider/js/extensions/revolution.extension.actions.min.js"></script>
+    <script src="../js/revolution-slider/js/extensions/revolution.extension.carousel.min.js"></script>
+    <script src="../js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js"></script>
+    <script src="../js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script src="../js/revolution-slider/js/extensions/revolution.extension.migration.min.js"></script>
+    <script src="../js/revolution-slider/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script src="../js/revolution-slider/js/extensions/revolution.extension.parallax.min.js"></script>
+    <script src="../js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script src="../js/revolution-slider/js/extensions/revolution.extension.video.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBOulZLrKOZcxCbyM01pXj9tM3iN-iST_Q&callback=initMap">
+    </script>
+    <script type="text/javascript">
+    var locations = [
+      ['India', 17.744649, 83.308444, 3]
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 2,
+      center: new google.maps.LatLng(17.92, 13.25),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) { 
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+  </script>
+    
+</body>
+
+</html>
