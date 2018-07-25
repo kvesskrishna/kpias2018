bf8e0b55547f2f2225a26812984d1fb1f5142f39
SatyaVache21CSS <satya@21cssindia.com>
2018-07-25 17:40:14 +0530

    prelims in direct ias

---
+++ courses/civils/direct-ias/prelims.php
@@ -0,0 +1,237 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+
+  <!-- Meta Tags -->
+  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <meta name="description" content="KPIAS" />
+  <meta name="keywords" content="education,school,university,educational,learn,learning,teaching,workshop" />
+  <meta name="author" content="KPIAS, 21st Century Software Solutions" />
+
+  <!-- Page Title -->
+  <title>KPIAS - Krishna Pradeep's IAS</title>
+
+  <!-- Favicon and Touch Icons -->
+  <link href="../../../images/logo.png" rel="shortcut icon" type="image/png">
+  <link href="../../../images/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="../../../images/apple-touch-icon-72x72.png" rel="apple-touch-icon" sizes="72x72">
+  <link href="../../../images/apple-touch-icon-114x114.png" rel="apple-touch-icon" sizes="114x114">
+  <link href="../../../images/apple-touch-icon-144x144.png" rel="apple-touch-icon" sizes="144x144">
+
+  <!-- Stylesheet -->
+  <link href="../../../css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="../../../css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+  <link href="../../../css/animate.css" rel="stylesheet" type="text/css">
+  <link href="../../../css/css-plugin-collections.css" rel="stylesheet" />
+  <!-- CSS | menuzord megamenu skins -->
+  <link href="../../../css/menuzord-megamenu.css" rel="stylesheet" />
+  <link id="menuzord-menu-skins" href="../../css/menuzord-skins/menuzord-boxed.css" rel="stylesheet" />
+  <!-- CSS | Main style file -->
+  <link href="../../../css/style-main.css" rel="stylesheet" type="text/css">
+  <!-- CSS | Preloader Styles -->
+  <link href="../../../css/preloader.css" rel="stylesheet" type="text/css">
+  <!-- CSS | Custom Margin Padding Collection -->
+  <link href="../../../css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
+  <!-- CSS | Responsive media queries -->
+  <link href="../../../css/responsive.css" rel="stylesheet" type="text/css">
+  <!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+  <!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
+
+  <!-- Revolution Slider 5.x CSS settings -->
+  <link href="../../../js/revolution-slider/css/settings.css" rel="stylesheet" type="text/css" />
+  <link href="../../../js/revolution-slider/css/layers.css" rel="stylesheet" type="text/css" />
+  <link href="../../../js/revolution-slider/css/navigation.css" rel="stylesheet" type="text/css" />
+
+  <!-- CSS | Theme Color -->
+  <link href="../../../css/colors/theme-skin-color-set1.css" rel="stylesheet" type="text/css">
+
+  <!-- external javascripts -->
+  <script src="../../../js/jquery-2.2.4.min.js"></script>
+  <script src="../../../js/jquery-ui.min.js"></script>
+  <script src="../../../js/bootstrap.min.js"></script>
+  <!-- JS | jquery plugin collection for this theme -->
+  <script src="../../../js/jquery-plugin-collection.js"></script>
+
+  <!-- Revolution Slider 5.x SCRIPTS -->
+  <script src="../../../js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
+  <script src="../../../js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!-- page level styles -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> 
+</head>
+
+<body class="has-side-panel side-panel-right fullwidth-page">
+  <div id="wrapper" class="clearfix">
+    <!-- preloader -->
+    <?php include '../../../preloader/preloader.php';?>
+    <!-- Header -->
+    <?php include '../../../header/header.php';?>
+    <!-- Start main-content -->
+    <div class="main-content">
+    <section class="inner-header divider layer-overlay overlay-theme-colored-7" data-bg-img="../../../images/bg/bg1.jpg">
+      <div class="container pt-120 pb-60">
+        <!-- Section Content -->
+        <div class="section-content">
+          <div class="row"> 
+            <div class="col-md-6">
+              <h2 class="text-theme-colored2 font-36">Prelims</h2>
+              <ol class="breadcrumb text-left mt-10 white">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Courses</a></li>
+                <li><a href="#">Civils</a></li>
+                <li class="active">Prelims</li>
+              </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <div class="container">
+        <div class="section-content">
+          <div class="row">
+            <div class="product">
+              <div class="col-md-12">
+                <div class="horizontal-tab product-tab">
+                  <ul class="nav nav-tabs">
+                    <li class="active"><a href="#tab1" data-toggle="tab">Description</a></li>
+                    <li><a href="#tab2" data-toggle="tab">Additional Information</a></li>
+                    <li><a href="#tab3" data-toggle="tab">Reviews</a></li>
+                  </ul>
+                  <div class="tab-content">
+                    <div class="tab-pane fade in active" id="tab1">
+                      <h3>Prelims Examination (Screening Test)</h3>
+                      <p>The preliminary exam is a screening test which is conducted to shortlist the candidates for the next stage of examination. The exam consists of one objective type paper. Minimum qualifying marks will be given for reservation candidates as per the rules.<br>
+
+      <ul style="list-style-type: none">
+      <li>Subjects  : General Studies & Mental Ability</li>
+      <li>Type  : Objective Type</li>
+      <li>Marks : 150 Marks</li>
+      <li>Duration  : 2:30 hrs</li>
+    </ul></p>
+                    </div>
+                    <div class="tab-pane fade" id="tab2">
+                      <table class="table table-striped">
+                        <tbody>
+                          <tr>
+                            <th>Duration</th>
+                            <td><p>xx months</p></td>
+                          </tr>
+                          <tr>
+                            <th>Eligibility</th>
+                            <td><p>elig</p></td>
+                          </tr>
+                          <tr>
+                            <th>Fees</th>
+                            <td><p>variable</p></td>
+                          </tr>
+                          <tr>
+                            <th>Available Campus</th>
+                            <td>Hyderabad, Rajahmundry, Vizag</td>
+                          </tr>
+                          <tr>
+                            <th>Facilities</th>
+                            <td>Campus, Transport, Hostel etc.</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <div class="tab-pane fade" id="tab3">
+                      <div class="reviews">
+                        <ol class="commentlist">
+                          <li class="comment">
+                            <div class="media"> <a href="#" class="media-left"><img class="thumb img-circle media-object" alt="" src="https://placehold.it/75x75" width="75"></a>
+                              <div class="media-body">
+                                <ul class="review_text list-inline mt-5">
+                                  <li>
+                                    <div title="Rated 5.00 out of 5" class="star-rating"><span  data-width="100%">5.00</span></div>
+                                  </li>
+                                  <li>
+                                    <h5 class="media-heading meta"><span class="author">Srinivas</span> – Mar 15, 2015:</h5>
+                                  </li>
+                                </ul>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat purus tempor sem molestie, sed blandit lacus posuere. Lorem ipsum dolor sit amet.</div>
+                            </div>
+                          </li>
+                          <li class="comment">
+                            <div class="media"> <a href="#" class="media-left"><img class="thumb img-circle media-object" alt="" src="https://placehold.it/75x75" width="75"></a>
+                              <div class="media-body">
+                                <ul class="review_text list-inline mt-5">
+                                  <li>
+                                    <div title="Rated 4.00 out of 5" class="star-rating"><span  data-width="80%">4.00</span></div>
+                                  </li>
+                                  <li>
+                                    <h5 class="media-heading meta"><span class="author">Yadav</span> – Jan 23, 2015:</h5>
+                                  </li>
+                                </ul>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat purus tempor sem molestie, sed blandit lacus posuere. Lorem ipsum dolor sit amet.</div>
+                            </div>
+                          </li>
+                        </ol>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-12 mt-30">
+              <h4>Related Courses</h4>
+              <div class="products related owl-carousel-4col" data-nav="true">
+                <div class="item">
+                  <div class="product">
+                    <div class="product-thumb"> <img alt="" src="../../../images/course/sm3.jpg" class="img-responsive img-fullwidth">
+                      <div class="overlay"></div>
+                    </div>
+                    <div class="product-details text-center">
+                      <a href="#"><h5 class="product-title">Degree with IAS</h5></a>
+                      <div class="price"><ins><span class="amount">Civils</span></ins></div>
+                      <div class="btn-add-to-cart-wrapper">
+                        <a class="btn btn-default btn-xs btn-add-to-cart" href="#">More..</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>    </div>
+    <!-- end main-content -->
+    <!-- Footer -->
+    <?php include '../../../footer/footer.php';?>      
+
+  </div>
+  <!-- end wrapper -->
+
+  <!-- Footer Scripts -->
+  <!-- JS | Chart-->
+  <script src="../../../js/chart.js"></script>
+  <!-- JS | Custom script for all pages -->
+  <script src="../../../js/custom.js"></script>
+
+  <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  
+      (Load Extensions only on Local File Systems ! 
+       The following part can be removed on Server for On Demand Loading) -->
+  <script src="../../../js/revolution-slider/js/extensions/revolution.extension.actions.min.js"></script>
+  <script src="../../../js/revolution-slider/js/extensions/revolution.extension.carousel.min.js"></script>
+  <script src="../../../js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js"></script>
+  <script src="../../../js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js"></script>
+  <script src="../../../js/revolution-slider/js/extensions/revolution.extension.migration.min.js"></script>
+  <script src="../../../js/revolution-slider/js/extensions/revolution.extension.navigation.min.js"></script>
+  <script src="../../../js/revolution-slider/js/extensions/revolution.extension.parallax.min.js"></script>
+  <script src="../../../js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js"></script>
+  <script src="../../../js/revolution-slider/js/extensions/revolution.extension.video.min.js"></script>
+
+</body>
+
+</html>
