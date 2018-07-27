54a9eec88797cff4b5f42df8263a02d6d75687a7
SatyaVache21CSS <satya@21cssindia.com>
2018-07-27 18:14:55 +0530

    content update

--- home/courses-section.php
+++ home/courses-section.php
@@ -17,7 +17,7 @@
                   <div class="item">
                     <div class="course-single-item bg-white border-1px clearfix">
                       <div class="course-thumb">
-                        <img class="img-fullwidth" alt="" src="../images/course/sm4.jpg">
+                        <img class="img-fullwidth" alt="" src="../images/course/Inter-IAS.jpg">
                       </div>
                       <div class="course-details clearfix p-20 pt-15">
                         <div class="course-top-part">
@@ -26,8 +26,11 @@
                           </a>
                           <h5 class="text-gray font-12 mt-0">CIVILS</h5>
                         </div>
-                        <p class="course-description mt-15 mb-0">Lorem ipsum dolor sit amet, consec sectet urad cing elit. Nobis commodi esse aliquam eligend reprehenderit,
-                          numquam a odio</p>
+                        <p class="course-description mt-15 mb-0">
+                          
+                            Krishna Pradeep’s 21st Century IAS Academy Provides integrated Civils coaching.!
+                        </p>
+                        <br>
                         <div class="author-thumb">
                           <img src="../images/logo.png" alt="" class="img-circle">
                         </div>
@@ -57,8 +60,7 @@
                           </a>
                           <h5 class="text-gray font-12 mt-0">CIVILS</h5>
                         </div>
-                        <p class="course-description mt-15 mb-0">Lorem ipsum dolor sit amet, consec sectet urad cing elit. Nobis commodi esse aliquam eligend reprehenderit,
-                          numquam a odio</p>
+                        <p class="course-description mt-15 mb-0">Normally aspirants start for Civil Services exam preparation after their post graduation at around 22-23 years of age</p>
                         <div class="author-thumb">
                           <img src="../images/logo.png" alt="" class="img-circle">
                         </div>
@@ -66,7 +68,7 @@
                       <div class="course-meta">
                         <ul class="list-inline">
                           <li>
-                            <i class="ficon-clock font-18"></i> 5 Years</li>
+                            <i class="ficon-clock font-18"></i> 3 Years</li>
                         </ul>
                         <div class="course-tag">
                           <a href="#" class="btn btn-warning btn-sm">More</a>
@@ -88,8 +90,8 @@
                           </a>
                           <h5 class="text-gray font-12 mt-0">Direct IAS Batch</h5>
                         </div>
-                        <p class="course-description mt-15 mb-0">Lorem ipsum dolor sit amet, consec sectet urad cing elit. Nobis commodi esse aliquam eligend reprehenderit,
-                          numquam a odio</p>
+                        <p class="course-description mt-15 mb-0">We have a regular IAS Batch, Please refer the timetable below</p>
+                          <br>
                         <div class="author-thumb">
                           <img src="../images/logo.png" alt="" class="img-circle">
                         </div>
@@ -97,7 +99,7 @@
                       <div class="course-meta">
                         <ul class="list-inline">
                           <li>
-                            <i class="ficon-clock font-18"></i> 6 Months</li>
+                            <i class="ficon-clock font-18"></i>1 Year</li>
                         </ul>
                         <div class="course-tag">
                           <a href="#" class="btn btn-warning btn-sm">More</a>
@@ -119,8 +121,7 @@
                           </a>
                           <h5 class="text-gray font-12 mt-0">Direct IAS Batch</h5>
                         </div>
-                        <p class="course-description mt-15 mb-0">Lorem ipsum dolor sit amet, consec sectet urad cing elit. Nobis commodi esse aliquam eligend reprehenderit,
-                          numquam a odio</p>
+                        <p class="course-description mt-15 mb-0">We have a meticulously designed weekend batches for the working professionals. It covers any one of the BBC as required by the candidates.</p>
                         <div class="author-thumb">
                           <img src="../images/logo.png" alt="" class="img-circle">
                         </div>
@@ -329,8 +330,7 @@
                           </a>
                           <h5 class="text-gray font-12 mt-0">Telangana</h5>
                         </div>
-                        <p class="course-description mt-15 mb-0">Lorem ipsum dolor sit amet, consec sectet urad cing elit. Nobis commodi esse aliquam eligend reprehenderit,
-                          numquam a odio</p>
+                        <p class="course-description mt-15 mb-0">The Telangana State PSC Group – I MainsExams Previously has only 5 Papers, now it will be 6 papers, 6th paper contains Telangana state information</p>
                         <div class="author-thumb">
                           <img src="../images/logo.png" alt="" class="img-circle">
                         </div>
