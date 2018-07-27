86bd3484027a8cdbe8585e28c3579131766e2014
kvesskrishna <krishna.kkarthi@gmail.com>
2018-07-27 11:05:54 +0530

    added gallery

--- header/header.php
+++ header/header.php
@@ -1,113 +1,124 @@
-    <header id="header" class="header">
-     
-      <div class="header-nav">
-        <div class="header-nav-wrapper navbar-scrolltofixed bg-white">
-          <div class="container">
-            <nav id="menuzord-right" class="menuzord default theme-colored">
-              <a class="menuzord-brand pull-left flip mt-20 mt-sm-10 mb-sm-20 pt-5" href="index-mp-layout1.html">
-                <img src="/images/logo.png" alt="" style = "max-height: 58px;">
-                <span style = "font-family: 'Montserrat', sans-serif;color:#3fa2b2;font-size:12px">Krishna Pradeep's IAS</span>
-              </a>
-              <ul class="menuzord-menu">
-                <li class="active">
-                  <a href="/home">Home</a>
-                </li>
+<header id="header" class="header">
+
+  <div class="header-nav">
+    <div class="header-nav-wrapper navbar-scrolltofixed bg-white">
+      <div class="container">
+        <nav id="menuzord-right" class="menuzord default theme-colored">
+          <a class="menuzord-brand pull-left flip mt-20 mt-sm-10 mb-sm-20 pt-5" href="index-mp-layout1.html">
+            <img src="/images/logo.png" alt="" style="max-height: 58px;">
+            <span style="font-family: 'Montserrat', sans-serif;color:#3fa2b2;font-size:12px">Krishna Pradeep's IAS</span>
+          </a>
+          <ul class="menuzord-menu">
+            <li class="active">
+              <a href="/home">Home</a>
+            </li>
+            <li>
+              <a href="/courses">Courses</a>
+              <ul class="dropdown">
                 <li>
-                  <a href="/courses">Courses</a>
+                  <a href="/courses/civils">Civils</a>
                   <ul class="dropdown">
-                  <li>
-                      <a href="/courses/civils">Civils</a>
+                    <li>
+                      <a href="/courses/civils/inter-with-ias">Inter with IAS</a>
+                    </li>
+                    <li>
+                      <a href="/courses/civils/degree-with-ias">Degree with IAS</a>
+                    </li>
+                    <li>
+                      <a href="/courses/civils/direct-ias">Direct IAS</a>
                       <ul class="dropdown">
-                      <li>
-                          <a href="/courses/civils/inter-with-ias">Inter with IAS</a>
+                        <li>
+                          <a href="/courses/civils/direct-ias/prelims">Prelims</a>
                         </li>
                         <li>
-                          <a href="/courses/civils/degree-with-ias">Degree with IAS</a>
+                          <a href="/courses/civils/direct-ias/mains">Mains</a>
                         </li>
                         <li>
-                          <a href="/courses/civils/direct-ias">Direct IAS</a>
-                          <ul class="dropdown">
-                            <li>
-                              <a href="/courses/civils/direct-ias/prelims">Prelims</a>
-                            </li>
-                            <li>
-                              <a href="/courses/civils/direct-ias/mains">Mains</a>
-                            </li>
-                            <li>
-                              <a href="/courses/civils/direct-ias/pcm">PCM Batches</a>
-                            </li>
-                          </ul>
-                        </li>
-                        <li>
-                          <a href="/courses/civils/repeaters-intensive-batch">Repeaters Intensive Batch</a>
-                          <ul class="dropdown">
-                            <li>
-                              <a href="/courses/civils/repeaters-intensive-batch/prelims">Prelims</a>
-                            </li>
-                            <li>
-                              <a href="/courses/civils/repeaters-intensive-batch/mains">Mains</a>
-                            </li>
-                            <li>
-                              <a href="/courses/civils/repeaters-intensive-batch/pcm">PCM Batches</a>
-                            </li>
-                            <li>
-                              <a href="/courses/civils/repeaters-intensive-batch/test-series">Test Series</a>
-                            </li>
-                          </ul>
+                          <a href="/courses/civils/direct-ias/pcm">PCM Batches</a>
                         </li>
                       </ul>
-                      
                     </li>
-                  <li>
-                      <a href="/courses/groups">Groups</a>
+                    <li>
+                      <a href="/courses/civils/repeaters-intensive-batch">Repeaters Intensive Batch</a>
                       <ul class="dropdown">
-                      <li>
-                          <a href="/courses/groups/group-1">Group 1</a>
-                          <ul class="dropdown">
-                            <li>
-                              <a href="/courses/groups/group-1/ap">Andhra Pradesh</a>
-                            </li>
-                            <li>
-                              <a href="/courses/groups/group-1/ts">Telangana</a>
-                            </li>
-                          </ul>
+                        <li>
+                          <a href="/courses/civils/repeaters-intensive-batch/prelims">Prelims</a>
                         </li>
                         <li>
-                          <a href="/courses/groups/group-2">Group 2</a>
-                          <ul class="dropdown">
-                            <li>
-                              <a href="/courses/groups/group-2/ap">Andhra Pradesh</a>
-                            </li>
-                            <li>
-                              <a href="/courses/groups/group-2/ts">Telangana</a>
-                            </li>
-                          </ul>
+                          <a href="/courses/civils/repeaters-intensive-batch/mains">Mains</a>
                         </li>
-
+                        <li>
+                          <a href="/courses/civils/repeaters-intensive-batch/pcm">PCM Batches</a>
+                        </li>
+                        <li>
+                          <a href="/courses/civils/repeaters-intensive-batch/test-series">Test Series</a>
+                        </li>
                       </ul>
                     </li>
                   </ul>
-                </li>
-                
-                <li>
-                  <a href="/about">About</a>
+
                 </li>
                 <li>
-                  <a href="/achievements">Achievements</a>
-                </li>
-               
-                <li>
-                  <a href="/gallery">Gallery</a>
-                </li>
-                <li>
-                  <a href="#">Student Zone <span class="label label-danger">Featured</span></a>
-                </li>
-                <li>
-                  <a href="/contact">Contact</a>
+                  <a href="/courses/groups">Groups</a>
+                  <ul class="dropdown">
+                    <li>
+                      <a href="/courses/groups/group-1">Group 1</a>
+                      <ul class="dropdown">
+                        <li>
+                          <a href="/courses/groups/group-1/ap">Andhra Pradesh</a>
+                        </li>
+                        <li>
+                          <a href="/courses/groups/group-1/ts">Telangana</a>
+                        </li>
+                      </ul>
+                    </li>
+                    <li>
+                      <a href="/courses/groups/group-2">Group 2</a>
+                      <ul class="dropdown">
+                        <li>
+                          <a href="/courses/groups/group-2/ap">Andhra Pradesh</a>
+                        </li>
+                        <li>
+                          <a href="/courses/groups/group-2/ts">Telangana</a>
+                        </li>
+                      </ul>
+                    </li>
+
+                  </ul>
                 </li>
               </ul>
-            </nav>
-          </div>
-        </div>
+            </li>
+
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/achievements">Achievements</a>
+            </li>
+
+            <li>
+              <a href="/gallery">Gallery</a>
+              <ul class="dropdown">
+                <li>
+                  <a href="/gallery/images">Image Gallery</a>
+                </li>
+                <li>
+                  <a href="/gallery/videos">Video Gallery</a>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <a href="#">Student Zone
+                <span class="label label-danger">Featured</span>
+              </a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
       </div>
-    </header>
+    </div>
+  </div>
+</header>
