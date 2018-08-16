ea51b4fe271ba9f6d6e4d0a4f23bd6b3a8c2fb08
SatyaVache21CSS <satya@21cssindia.com>
2018-08-16 18:06:48 +0530

    testimonials
    made few changes to what sandeep sent and kept

--- home/testimonials.php
+++ home/testimonials.php
@@ -16,9 +16,7 @@
                 <div class="item">
                   <div class="testimonial pt-10">
                     <div class="testimonial-content">
-                      <p class="mt-0 font-16">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas vel sint, ut. Quisquam doloremque minus
-                        possimus eligendi dolore ad.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas vel sint,
-                        ut. Quisquam doloremque minus possimus eligendi dolore ad.Lorem ipsum dolor sit</p>
+                      <p class="mt-0 font-16">I opted Inter with IAS course in 21st Century IAS Academy before an year. Overall training for a year was very fruitful with the best guidance by the KP sir team and all the expertise faculty guided us in every aspect that we lag and provided us few simplified methods in memorizing all the concepts. Overall the training at 21st century IAS Academy has been worth taking and I would recommend others who are looking to pursue their career in IAS & Competitive exams. Go and opt  21st Century IAS Academy and it will be your one-stop solution.</p>
                     </div>
                     <div class="testimonial-details mb-0 mr-0">
                       <img alt="" src="images/testimonials/s1.jpg" class="img-thumbnail img-circle pull-left mr-15 mt-15" width="42">
@@ -33,9 +31,7 @@
                 <div class="item">
                   <div class="testimonial pt-10">
                     <div class="testimonial-content">
-                      <p class="mt-0 font-16">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas vel sint, ut. Quisquam doloremque minus
-                        possimus eligendi dolore ad.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas vel sint,
-                        ut. Quisquam doloremque minus possimus eligendi dolore ad.Lorem ipsum dolor sit</p>
+                      <p class="mt-0 font-16">Hi myself Vinay I joined in 21st Century IAS Academy 3 months back for the Degree with IAS course and here in the academy I have met few of the experienced IAS Faculty who guides us in a very effective manner and they also provide us tips how to practice exams & crack the exam in our first attempt. I would surely recommend the aspirants who are preparing for IAS or other competitive examinations like groups and UPPSC can get the best training from 21st Century IAS Academy.</p>
                     </div>
                     <div class="testimonial-details mb-0 mr-0">
                       <img alt="" src="images/testimonials/s2.jpg" class="img-thumbnail img-circle pull-left mr-15 mt-15" width="42">
@@ -49,9 +45,8 @@
                 <div class="item">
                   <div class="testimonial pt-10">
                     <div class="testimonial-content">
-                      <p class="mt-0 font-16">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas vel sint, ut. Quisquam doloremque minus
-                        possimus eligendi dolore ad.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas vel sint,
-                        ut. Quisquam doloremque minus possimus eligendi dolore ad.Lorem ipsum dolor sit</p>
+                      <p class="mt-0 font-16">
+As KPIAS has been the best and well-known academy all over India for its achievements and training in IAS, Groups & Other reputed competitive exams after knowing all these facts about kpias I have joined this academy and enrolled for Degree with IAS course. The team over there has given me a wonderful counselling & support among all the doubts related to the process and further proceedings.  After that, I have enrolled in the programme and found some real-time career guides who guided me through all the aspects in an effective manner. Which helped me to get a clear view on how to clear the examinations in an effective manner. Thank you team KPIAS for the wonderful training & guidance.</p>
                     </div>
                     <div class="testimonial-details mb-0 mr-0">
                       <img alt="" src="images/testimonials/s1.jpg" class="img-thumbnail img-circle pull-left mr-15 mt-15" width="42">
